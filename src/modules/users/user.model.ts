09246efd23f9ec4008b2eac5094b91ab4f34c845
nejcsakulac <nejc.sakulac@gmail.com>
2025-05-11 19:16:20 +0200

    connection to database, prisma setup, .env, user model

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,11 @@
+
+import { Prisma } from '@prisma/client';
+
+export class User implements Prisma.UserCreateInput {
+  first_name: string;
+  last_name: string;
+  avatar: string;
+  password: string;
+  email: string;
+  createdAt: Date;
+}
